45104617cba3977ed26f7c273807a2ec434d25bf
Alice Chan <alicebay10@yahoo.co.uk>
2024-09-18 19:57:05 +0000

    Added  excerpt & updated_on models in models.py & made migrations.

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alicec119-djangoblog-1b58pl0mu82.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
