fbd2d1d7e7c772344bf74b08b7f81f431840a441
Alice Chan <alicebay10@yahoo.co.uk>
2024-09-25 10:49:59 +0000

    Created the collaboration form in About page.

--- about/views.py
+++ about/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import About
+from .forms import CollaborateForm
 
 
 def about_me(request):
@@ -9,9 +10,11 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+        "collaborate_form": collaborate_form},
     )
