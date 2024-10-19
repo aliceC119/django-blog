3773bc72c15605422d99f9ffcc319cfffbfb9d8e
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-19 13:51:59 +0000

    Added like & comment icon to the home page and linked them to blog posts.

--- contact/forms.py
+++ contact/forms.py
@@ -6,6 +6,4 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('name', 'email', 'message')
-        name = forms.CharField(max_length=100)
-        email = forms.EmailField()
-        message = forms.CharField(widget=forms.Textarea)
+        
