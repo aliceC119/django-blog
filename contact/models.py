fcee7f8917bc52c063a9a1ecf025f8de8ee64bed
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-19 17:28:41 +0000

    Added testing codes for apps.

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    
     message = models.TextField()
 
     def __str__(self):
